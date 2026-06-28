487958416b2a5bfe57414c7823a83e17c73623df
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-06-28 09:36:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let btn = document.querySelectorAll('.btn');
+let stop = document.getElementByClassName('stop');
+let aud;
+btn.forEach((btn)=>{
+	btn.addEventListener('click',()=>{
+		let sound = btn.textContent;
+		aud = new Audio(`sounds/${sound}.wav`);
+		aud.play();
+	})
+})
+
+stop[0].addEventListener('click',()=>{
+	aud.pause();
+})
